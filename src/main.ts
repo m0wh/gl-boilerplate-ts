1f428c52b82708f0a1c8fb6a3cf97f3bd92120ed
m0wh <malo.dewiderspach@gmail.com>
2020-12-08 11:08:28 +0100

    frontend -> gl

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('hello')
+import { init } from './ts/utils/three-helpers'
+import raf from './ts/utils/raf'
+import fx from './ts/effects'
+import { BoxGeometry, Mesh, MeshNormalMaterial } from 'three'
+
+const { camera, renderer, scene } = init()
+renderer.pixelRatio = 2
+
+const { composer } = fx({ renderer, scene, camera })
+
+camera.position.z = 5
+camera.position.x = 5
+camera.position.y = 5
+
+const geo = new BoxGeometry(1, 1, 1)
+const mat = new MeshNormalMaterial()
+const box = new Mesh(geo, mat)
+scene.add(box)
+
+raf.subscribe((time) => {
+  box.rotation.y = time / 1000
+  composer.render()
+})
