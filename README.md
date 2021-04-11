afddb556433f425ed364966203a3ab6f7793da9d
m0wh <malo.dewiderspach@gmail.com>
2021-04-11 23:11:18 +0200

    update to parcel 2

--- README.md
+++ README.md
@@ -26,5 +26,4 @@ newgl () {
 yarn # install dependencies
 yarn start # run dev server
 yarn build # builds your site
-yarn deploy # builds and deploys your site to gh-pages branch
 ```
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { init } from './ts/utils/three-helpers'
 import raf from './ts/utils/raf'
 import fx from './ts/effects'
 import { BoxGeometry, Mesh, MeshNormalMaterial } from 'three'
+import { randomGenerator } from './ts/utils/helpers'
+
+const random = randomGenerator() // put seed here
 
 const { camera, renderer, scene } = init()
 renderer.pixelRatio = 2
@@ -12,7 +15,7 @@ camera.position.z = 5
 camera.position.x = 5
 camera.position.y = 5
 
-const geo = new BoxGeometry(1, 1, 1)
+const geo = new BoxGeometry(random() * 1 + 1, random() * 1 + 1, random() * 1 + 1)
 const mat = new MeshNormalMaterial()
 const box = new Mesh(geo, mat)
 scene.add(box)
